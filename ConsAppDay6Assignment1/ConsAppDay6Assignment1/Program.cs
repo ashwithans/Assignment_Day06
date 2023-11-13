70ae4165d77c6ae6f9d3b9f805d6de1eb265adb7
ashwithans <ashwithans@gmail.com>
2023-11-13 15:03:44 +0530

    Assignment_Day06

---
+++ ConsAppDay6Assignment1/ConsAppDay6Assignment1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsAppDay6Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a jagged array for student scores
+            int[][] studentScores = new int[3][];
+
+            // Initialize the jagged array with scores for each student
+            studentScores[0] = new int[] { 85, 92, 78 };
+            studentScores[1] = new int[] { 90, 87, 93, 89 };
+            studentScores[2] = new int[] { 76, 88 };
+
+            // Iterate through the jagged array to print scores for each student and calculate their average scores
+            for (int student = 0; student < studentScores.Length; student++)
+            {
+                int[] scores = studentScores[student];
+                Console.WriteLine("Student " + (student + 1) + " scores:");
+                int totalScore = 0;
+
+                foreach (int score in scores)
+                {
+                    Console.Write(score + " ");
+                    totalScore += score;
+                }
+
+                double averageScore = (double)totalScore / scores.Length;
+                Console.WriteLine("\nAverage score for Student " + (student + 1) + ": " + averageScore);
+            }
+
+            // Calculate the average score for all students combined
+            int totalAllScores = 0;
+            int totalAllStudents = 0;
+
+            foreach (int[] scores in studentScores)
+            {
+                totalAllScores += scores.Length;
+                totalAllStudents += scores.Sum();
+            }
+
+            double averageAllStudents = (double)totalAllStudents / totalAllScores;
+            Console.WriteLine("Average score for all students combined: " + averageAllStudents);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsAppDay6Assignment2/ConsAppDay6Assignment2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsAppDay6Assignment2
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee Salary Calculation System");
+            Console.WriteLine("Choose Employee Type:");
+            Console.WriteLine("1. HR");
+            Console.WriteLine("2. Admin");
+            Console.WriteLine("3. Software Developer");
+            Console.WriteLine("choose any one from the above choices");
+            int employeeType = int.Parse(Console.ReadLine());
+
+
+            switch (employeeType)
+            {
+                case 1: // HR
+                    Console.Write("Enter working hours: ");
+                    int workingHours = int.Parse(Console.ReadLine());
+                    Console.Write("Enter number of working days: ");
+                    int workingDays = int.Parse(Console.ReadLine());
+                    double salary = CalculateSalary(workingHours, workingDays);
+                    Console.WriteLine("Monthly salary for HR: " + salary);
+                    break;
+
+                case 2: // Admin
+                    Console.Write("Enter working hours: ");
+                    int adminWorkingHours = int.Parse(Console.ReadLine());
+                    Console.Write("Enter number of working days: ");
+                    int adminWorkingDays = int.Parse(Console.ReadLine());
+                    Console.Write("Enter number of project handles: ");
+                    int projectHandles = int.Parse(Console.ReadLine());
+                    salary = CalculateSalary(adminWorkingHours, adminWorkingDays, projectHandles: projectHandles);
+                    Console.WriteLine("Monthly salary for Admin: " + salary);
+                    break;
+
+                case 3: // Software Developer
+                    Console.Write("Enter working hours: ");
+                    int devWorkingHours = int.Parse(Console.ReadLine());
+                    Console.Write("Enter number of working days: ");
+                    int devWorkingDays = int.Parse(Console.ReadLine());
+                    Console.Write("Enter number of project handles: ");
+                    int devProjectHandles = int.Parse(Console.ReadLine());
+                    Console.Write("Enter extras: ");
+                    int extras = int.Parse(Console.ReadLine());
+                    salary = CalculateSalary(devWorkingHours, devWorkingDays, devProjectHandles, extras);
+                    Console.WriteLine("Monthly salary for Software Developer: " + salary);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    break;
+            }
+
+        }
+
+        static double CalculateSalary(int workingHours, int workingDays, int projectHandles = 0, int extras = 0)
+        {
+            return workingHours * workingDays * 100 + projectHandles * 3000 + extras * 2000;
+        }
+        
+       
+    }
+}
